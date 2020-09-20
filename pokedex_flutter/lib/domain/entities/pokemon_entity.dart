18bddc766f21e3170849f44f116e2e7a721cb30d
Diogo Guilherme da Silva Santos <dgssantos@hotmail.com>
2020-09-19 23:35:18 -0300

    reafctor: change pokemon class to a domain entity

---
+++ pokedex_flutter/lib/domain/entities/pokemon_entity.dart
@@ -0,0 +1,8 @@
+class PokemonEntity {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final String type;
+
+  PokemonEntity({this.id, this.name, this.imageUrl, this.type});
+}
--- pokedex_flutter/lib/domain/pokemon.dart
+++
@@ -1,8 +0,0 @@
-class Pokemon {
-  final int id;
-  final String name;
-  final String imageUrl;
-  final String type;
-
-  Pokemon({this.id, this.name, this.imageUrl, this.type});
-}
